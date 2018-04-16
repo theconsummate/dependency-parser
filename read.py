9f92147932a3a531587204e96984f5f55653bf74
Dhruv Mishra <ubiquitiousdm@gmail.com>
2018-04-16 16:30:55 +0200

    adding root token

--- read.py
+++ read.py
@@ -1,17 +1,18 @@
 def load_conll_file(file):
     f = open(file)
     sentences = []
-    tokens = []
+    tokens = [Token.get_root_token()]
     for line in f.readlines():
         # remove the new line char first
         line = line.strip()
         if line == "":
             # the sentence has ended
             sentences.append(tokens)
-            tokens = []
+            tokens = [Token.get_root_token()]
+            print tokens[0]
         else:
             token = Token(line)
-            # print token
+            print token
             tokens.append(token)
     return sentences
 
@@ -33,3 +34,7 @@ class Token():
     
     def __str__(self):
         return "{id=" + str(self.id) + ",form=" + self.form + ",head=" + str(self.head) + ",deprel=" + self.deprel + "}"
+    
+    @staticmethod
+    def get_root_token():
+        return Token("0\t<ROOT>\t<ROOT>\t<ROOT>\t_\t_\t-1\tROOT\t_\t_") 
